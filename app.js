28aaa57906ed29b3e39b50ceadef28abcd5570e7
Nikunj Rathod <nikunjrathod3011@gmail.com>
2020-01-20 00:27:07 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+new Vue({
+    el:'#vue-app',
+    data:{
+        name:'Helly',
+        greet:'Ohayu',
+        website:'https://nikunj3011.github.io',
+        websitetag:'<a href="https://nikunj3011.github.io">Me- blog</a>',
+        age:10,
+        x:0,
+        y:0
+    },
+    methods:{
+        greets:function(){
+            return 'Bello';
+        },
+        greett:function(time){
+            return 'Good' +time+' ,'+ this.name;
+        },
+        add:function(inc){
+            this.age+=inc;
+        },
+        sub:function(dec){
+            this.age-=dec;
+        },
+        updateXY:function(event){
+            this.x=event.offsetX;
+            this.y=event.offsetY;           
+
+        }
+    }
+});
